6113065e166240f0348919ea46356b00c133356b
Bao1405 <lqbbiker15052@gmail.com>
2024-06-19 11:55:02 +0700

    update

---
+++ ss67/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ManagerLibrary from './components/manager'
+
+export default function App() {
+  return (
+    <div>
+      <ManagerLibrary></ManagerLibrary>
+    </div>
+  )
+}
